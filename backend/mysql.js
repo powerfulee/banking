9f1d2300fc72533be8f1908e58ae33879d2f2198
feelingatfall@gmail.com <feelingatfall@gmail.com>
2018-09-29 18:13:19 +0800

    modified database name.

--- backend/mysql.js
+++ backend/mysql.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
 	host : '192.168.3.40',
 	user : 'sycf',
 	password : '000000',
-	database : 'homestead',
+	database : 'banking',
 	port : '3306',
 	charset : 'UTF8_GENERAL_CI'
 })
