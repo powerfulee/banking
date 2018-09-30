7fb264a2c8060bd4c602d60f0250d856e3f574a6
feelingatfall@gmail.com <feelingatfall@gmail.com>
2018-09-30 15:00:33 +0800

    update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/page/index'
-import List from '@/page/list'
-import Detail from '@/page/detail'
 
 Vue.use(Router)
 
@@ -11,22 +9,6 @@ export default new Router({
     {
       path: '/',
       component: Index,
-    },
-    {
-      path: '/list',
-      redirect: '/list/:id',
-      component: List,
-      children: [
-        { path: ':id', name: 'list', component: List }
-      ]
-    },
-    {
-      path: '/detail',
-      redirect: '/detail/:id',
-      component: Detail,
-      children: [
-        { path: ':id', name: 'detail', component: Detail }
-      ]
     }
   ]
 })
