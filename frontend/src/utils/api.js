90fabd5b6c59fde4b97c2523662b4ee54c8b2ec7
feelingatfall@gmail.com <feelingatfall@gmail.com>
2018-09-30 11:51:17 +0800

    update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -24,15 +24,6 @@ function filterNull (o) {
   }
   return o
 }
-/*
-  接口处理函数
-  这个函数每个项目都是不一样的，我现在调整的是适用于
-  https://cnodejs.org/api/v1 的接口，如果是其他接口
-  需要根据接口的参数进行调整。参考说明文档地址：
-  https://cnodejs.org/topic/5378720ed6e2d16149fa16bd
-  主要是，不同的接口的成功标识和失败提示是不一致的。
-  另外，不同的项目的处理方法也是不一致的，这里出错就是简单的alert
-*/
 
 function apiAxios (method, url, params, success, failure) {
   if (params) {
